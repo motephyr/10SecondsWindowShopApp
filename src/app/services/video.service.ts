6baa97dbb5fd02c9ad8853403b87f241f9abc6a4
motephyr <motephyr@gmail.com>
2021-02-13 17:50:31 +0800

    Try to send video file

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,6 +1,11 @@
-import {Injectable} from '@angular/core';
-import {MediaCapture, MediaFile, CaptureError, CaptureVideoOptions} from '@ionic-native/media-capture/ngx';
+import { Injectable } from '@angular/core';
+import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions } from '@ionic-native/media-capture/ngx';
+import { HttpClient } from '@angular/common/http';
+import { Capacitor } from '@capacitor/core';
 
+import { File } from '@ionic-native/file/ngx';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,32 +14,57 @@ export class VideoService {
   files = [];
 
 
-  constructor(private mediaCapture: MediaCapture
-  ) {}
+  constructor(private mediaCapture: MediaCapture, private http: HttpClient) {}
 
-
-
-  copyFileToLocalDir(fullPath) {
-    let myPath = fullPath;
-    // Make sure we copy from the right location
-    if (fullPath.indexOf('file://') < 0) {
-      myPath = 'file://' + fullPath;
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}\n${error.error}`;
     }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 
-    console.log('myPath');
-    console.log(myPath);
+  async copyFileToLocalDir(data) {
 
+    const blob = await fetch(
+      Capacitor.convertFileSrc(data.fullPath)
+    ).then(r => r.blob());
 
+    const formData = new FormData();
+    formData.append('title', 'test');
+    formData.append('price', '100');
+    formData.append('email', 'motephyr@gmail.com');
+    formData.append('video_file', blob, data.name);
+
+    const headers = {
+      'enctype': 'multipart/form-data',
+      'Accept': 'plain/text'
+    };
+    let options = { headers: headers }
+    let url: any = 'https://windowshop.leanstep.net/v1/items';
+    let a = this.http.post(url, formData, options).pipe(
+      catchError(this.handleError)
+    )
+    a.subscribe((data) => {
+      console.log('data');
+      console.log(data);
+    })
+    console.log('end')
   }
 
 
   public async addNewToGallery() {
-    let options: CaptureVideoOptions = {limit: 1, duration: 10}
-    this.mediaCapture.captureVideo(options)
+    let options: CaptureVideoOptions = { limit: 1, duration: 10, quality:100 }
+    await this.mediaCapture.captureVideo(options)
       .then(
-        (data: MediaFile[]) => {
+        async (data: MediaFile[]) => {
           if (data.length > 0) {
-            this.copyFileToLocalDir(data[0].fullPath);
+            await this.copyFileToLocalDir(data[0]);
           }
         },
         (err: CaptureError) => {
